35e58db85b7f935b39cf76f9f402841ae6a3cf0d
bsod <aparin@alkor.co.ru>
2022-05-16 21:26:57 +0500

    Initial commit

---
+++ app/src/main/java/com/als/gbnotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.als.gbnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
