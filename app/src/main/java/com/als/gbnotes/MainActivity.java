353e70230218e2e6f92ef48c1d088e19f86e3c5f
bsod <aparin@alkor.co.ru>
2022-05-23 20:37:10 +0500

    Initial commit notes

--- app/src/main/java/com/als/gbnotes/MainActivity.java
+++ app/src/main/java/com/als/gbnotes/MainActivity.java
@@ -1,8 +1,9 @@
 package com.als.gbnotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CitiesFragment citiesFragment = new CitiesFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, citiesFragment)
+                .commit();
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            CoatOfArmsFragment coaf = CoatOfArmsFragment.newInstance(0);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.coat_of_arms_container, coaf)
+                    .commit();
+        }
+
     }
 }
